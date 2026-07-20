e62d2f81fde5a789d0f2ee7dffcebc10b61b76dd
Tai. Tani. <62682789+tai-calg@users.noreply.github.com>
2026-07-20 23:59:40 +0900

    Export Glyph Studio API

--- glyph/__init__.py
+++ glyph/__init__.py
@@ -11,6 +11,7 @@ from .frontend import compile_file, compile_source, parse_program
 from .incremental import CompilationSnapshot, IncrementalCompiler, IncrementalResult
 from .mermaid import DiagramBundle, compile_diagram_bundle, write_diagram_bundle
 from .semantic import SemanticModel
+from .studio import GlyphStudio, StudioSnapshot, run_studio
 from .symbols import SymbolId, SymbolRecord
 
 __all__ = [
@@ -18,10 +19,12 @@ __all__ = [
     "CompilationSnapshot",
     "DiagramBundle",
     "GlyphError",
+    "GlyphStudio",
     "IncrementalCompiler",
     "IncrementalResult",
     "RustArtifacts",
     "SemanticModel",
+    "StudioSnapshot",
     "SymbolId",
     "SymbolRecord",
     "compile_artifact_files",
@@ -32,5 +35,6 @@ __all__ = [
     "parse_artifact_model",
     "parse_compilation_model",
     "parse_program",
+    "run_studio",
     "write_diagram_bundle",
 ]
