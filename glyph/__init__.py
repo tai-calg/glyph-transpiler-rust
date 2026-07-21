6d45789ecdbd4f280fe171577d402b4a7f2121c5
Tai. Tani. <62682789+tai-calg@users.noreply.github.com>
2026-07-21 20:34:23 +0900

    Export raw preprocessor API

--- glyph/__init__.py
+++ glyph/__init__.py
@@ -10,6 +10,7 @@ from .compiler import GlyphError
 from .frontend import compile_file, compile_source, parse_program
 from .incremental import CompilationSnapshot, IncrementalCompiler, IncrementalResult
 from .mermaid import DiagramBundle, compile_diagram_bundle, write_diagram_bundle
+from .preprocessor import PreprocessResult, RawMacroDef, preprocess_source
 from .semantic import SemanticModel
 from .studio import GlyphStudio, StudioSnapshot, run_studio
 from .symbols import SymbolId, SymbolRecord
@@ -22,6 +23,8 @@ __all__ = [
     "GlyphStudio",
     "IncrementalCompiler",
     "IncrementalResult",
+    "PreprocessResult",
+    "RawMacroDef",
     "RustArtifacts",
     "SemanticModel",
     "StudioSnapshot",
@@ -35,6 +38,7 @@ __all__ = [
     "parse_artifact_model",
     "parse_compilation_model",
     "parse_program",
+    "preprocess_source",
     "run_studio",
     "write_diagram_bundle",
 ]
